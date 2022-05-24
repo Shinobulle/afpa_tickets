6dfe69317393e6a470f051debbd3dca5bef1685a
antoinevinciguerra <antoinevinciguerra@gmail.com>
2022-05-24 10:59:57 +0200

    Affichage des departments

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use Faker\Factory;
 use App\Entity\Ticket;
+use App\Entity\Department;
 use Doctrine\Persistence\ObjectManager;
 use App\Faker\Provider\ImmutableDateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -14,11 +15,31 @@ class AppFixtures extends Fixture
     {
         $faker = Factory::create('fr_FR');
 
+        // Création de 10 departments
+        for ($d = 0; $d < 10; $d++) {
+            // Création d'un nouvel objet department
+
+            $department = new Department;
+
+            // On nourrit l'objet department
+            $department->setName($faker->company());
+
+            // On persist notre objet department
+            $manager->persist($department);
+        }
+
+        // On push les departments en BDD 
+        $manager->flush();
+
+        $allDepartments = $manager->getRepository(Department::class)->findAll();
+
         // Création entre 30 et 50 tickets aléatoirement
         for ($t = 0; $t < mt_rand(30, 50); $t++) {
 
             // Création d'un nouveau ticket    
             $ticket = new Ticket;
+
+
 
             // On nourrit l'objet Ticket
             $ticket->setMessage($faker->paragraph(3))
@@ -26,7 +47,8 @@ class AppFixtures extends Fixture
                 ->setIsActive($faker->boolean(75))
                 ->setCreateAt(new \DateTimeImmutable())
                 ->setFinishedAt(!$ticket->getIsActive() ? ImmutableDateTime::immutableDateTimeBetween('now', '6 months') : null)
-                ->setObject($faker->sentence(6));
+                ->setObject($faker->sentence(6))
+                ->setDepartment($faker->randomElement($allDepartments));
 
             // On fait persister les données                  
             $manager->persist($ticket);
