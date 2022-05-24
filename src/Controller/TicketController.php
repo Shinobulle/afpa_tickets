4326a25b7c9f1d5d79011062fe9ca1157a4169a1
antoinevinciguerra <antoinevinciguerra@gmail.com>
2022-05-24 11:53:13 +0200

    Creation du formulaire ticketType

--- src/Controller/TicketController.php
+++ src/Controller/TicketController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 
+use App\Entity\Ticket;
+use App\Form\TicketType;
 use App\Repository\TicketRepository;
-
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -31,6 +33,20 @@ class TicketController extends AbstractController
 
         return $this->render('ticket/index.html.twig', [
             'tickets' => $tickets,
+        ]);
+    }
+
+    /**
+     * @Route("/ticket/create", name="ticket_create")
+     */
+    public function createTicket(Request $request)
+    {
+        $ticket = new Ticket;
+
+        $form = $this->createForm(TicketType::class, $ticket, []);
+
+        return $this->render('ticket/create.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
