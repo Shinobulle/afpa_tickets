4f03871e0c563615da85cce0301b911486f841fb
antoinevinciguerra <antoinevinciguerra@gmail.com>
2022-06-03 08:36:58 +0200

    📦 NEW: export pdf et excel

--- src/Controller/TicketController.php
+++ src/Controller/TicketController.php
@@ -3,16 +3,22 @@
 namespace App\Controller;
 
 
+use Dompdf\Dompdf;
+use Dompdf\Options;
 use App\Entity\Ticket;
 use App\Form\TicketType;
-use App\Repository\TicketRepository;
 use Psr\Log\LoggerInterface;
+use App\Controller\MailerController;
+use App\Repository\TicketRepository;
 use Symfony\Component\Workflow\Registry;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
@@ -161,5 +167,98 @@ class TicketController extends AbstractController
         $this->ticketRepository->add($ticket, true);
 
         return $this->redirectToRoute('app_ticket');
+    }
+
+    /**
+     * @Route("/pdf", name="ticket_pdf")
+     */
+    public function pdf(): Response
+    {
+        //Données utiles
+        $user = $this->getUser();
+        $tickets = $this->ticketRepository->findBy(['user' => $user]);
+
+        //Configuration de Dompdf
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Arial');
+
+        //Instantiation de Dompdf
+        $dompdf = new Dompdf($pdfOptions);
+
+        //Récupération du contenu de la vue
+        $html = $this->renderView('ticket/pdf.html.twig', [
+            'tickets' => $tickets,
+            'title' => "Bienvenue sur notre page PDF"
+        ]);
+
+        //Ajout du contenu de la vue dans le PDF
+        $dompdf->loadHtml($html);
+
+        //Configuration de la taille et de la largeur du PDF
+        $dompdf->setPaper('A4', 'portrait');
+
+        //Récupération du PDF
+        $dompdf->render();
+
+        //Création du fichier PDF
+        $dompdf->stream("ticket.pdf", [
+            "Attachment" => true
+        ]);
+
+        return new Response('', 200, [
+            'Content-Type' => 'application/pdf'
+        ]);
+    }
+
+    /**
+     * @Route("/excel", name="ticket_excel")
+     */
+    public function excel(): Response
+    {
+
+        //Données utiles
+        $user = $this->getUser();
+        $tickets = $this->ticketRepository->findBy(['user' => $user]);
+
+
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+        $sheet->setCellValue('A1', 'Liste des tickets pour l\'utilisateur : ' . $user->getUsername());
+        $sheet->mergeCells('A1:E1');
+        $sheet->setTitle("Liste des tickets");
+
+        //Set Column names
+        $columnNames = [
+            'Id',
+            'Objet',
+            'Date de création',
+            'Department',
+            'Statut',
+        ];
+        $columnLetter = 'A';
+        foreach ($columnNames as $columnName) {
+            $sheet->setCellValue($columnLetter . '3', $columnName);
+            $sheet->getColumnDimension($columnLetter)->setAutoSize(true);
+            $columnLetter++;
+        }
+        foreach ($tickets as $key => $ticket) {
+            $sheet->setCellValue('A' . ($key + 4), $ticket->getId());
+            $sheet->setCellValue('B' . ($key + 4), $ticket->getObject());
+            $sheet->setCellValue('C' . ($key + 4), $ticket->getCreateAt()->format('d/m/Y'));
+            $sheet->setCellValue('D' . ($key + 4), $ticket->getDepartment()->getName());
+            $sheet->setCellValue('E' . ($key + 4), $ticket->getTicketStatut());
+        }
+        //Création du fichier xlsx
+        $writer = new Xlsx($spreadsheet);
+
+        //Création d'un fichier temporaire
+        $fileName = "Export_Tickets.xlsx";
+        $temp_file = tempnam(sys_get_temp_dir(), $fileName);
+
+        //créer le fichier excel dans le dossier tmp du systeme
+        $writer->save($temp_file);
+
+        //Renvoie le fichier excel
+        return $this->file($temp_file, $fileName, ResponseHeaderBag::DISPOSITION_INLINE);
     }
 }
